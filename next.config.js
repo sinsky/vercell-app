b61507cf70b263a93cf5af4abc9c8340a6280c54
sinsky <60662802+sinsky@users.noreply.github.com>
2022-06-19 02:19:50 +0900

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/me",
+        destination: "https://sinsky.dev",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
